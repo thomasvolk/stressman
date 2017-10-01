739799ab4066d9a98079b451d25e55e39365ed55
Thomas Volk <info@thomasvolk.de>
2017-10-01 11:46:05 +0200

    handle errors and redirects

--- lib/worker.ex
+++ lib/worker.ex
@@ -7,8 +7,18 @@ defmodule Stress.Worker do
      handle_response({Duration.to_milliseconds(timestamp), response})
   end
 
-  defp handle_response({milliseconds, {:ok, %HTTPoison.Response{ status_code: code}}}) when code >= 200 and code <=304 do
-    {:ok, milliseconds}
+  defp handle_response({milliseconds, {:ok, %HTTPoison.Response{ status_code: code}}}) when code >= 200 and code < 300 do
+    {:ok, {code, milliseconds}}
+  end
+
+  defp handle_response({milliseconds, {:ok, %HTTPoison.Response{ status_code: code}}}) when code >= 300 and code < 400 do
+    Logger.info("redirect worker #{node()}-#{inspect self()}: #{code}")
+    {:redirect, {code, milliseconds}}
+  end
+
+  defp handle_response({milliseconds, {:ok, %HTTPoison.Response{ status_code: code}}}) do
+    Logger.error("error worker #{node()}-#{inspect self()}: #{code}")
+    {:error, {code, milliseconds}}
   end
 
   defp handle_response({_ms, {:error, reason}}) do
